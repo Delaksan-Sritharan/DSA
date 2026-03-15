1a6331531256ffb3bed71bebde313523731beee3
Delaksan-Sritharan <delaksan@gmail.com>
2026-03-15 20:58:17 +0530

    feat: added BinarySearch

---
+++ src/main/java/BinarySearch/BinarySearch.java
@@ -0,0 +1,34 @@
+package BinarySearch;
+
+public class BinarySearch {
+    public static int binarySearch(int arr[],int targetVal){
+        int left = 0;
+        int right = arr.length -1;
+
+        while (left<=right){
+            int mid = (left + right)/2;
+
+            if (arr[mid] == targetVal){
+                return mid;
+            }else if(arr[mid] < targetVal){
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+
+    }
+    public static void main(String[] args) {
+        int [] arr = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        int targetVal = 15;
+
+        int result = binarySearch(arr, targetVal);
+
+        if (result!= -1){
+            System.out.println("Value is found.");
+        }else {
+            System.out.println("Value is not found. ");
+        }
+    }
+}
